13e3b8a8e0df53308f4d00ebba9aa326d32ad9a1
nv21064-salman-ali <118216333+nv21064-salman-ali@users.noreply.github.com>
2022-12-26 11:15:19 +0300

    update article title

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -45,7 +45,7 @@ export default function ArticlesIndex({ articles }) {
         />
       </Head>
       <SimpleLayout
-        title=" on Cloud Engineer, company building, and the aerospace industry."
+        title="Cloud Engineer"
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
